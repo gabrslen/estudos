f0deafad70d84c523cd4646485f36e3597580671
Gabriel <gabrielgustavotavares@gmail.com>
2021-03-28 12:00:23 -0300

    Pasta Pandas Adicionada

---
+++ Pandas/InputList.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+n=int(input("Students "))
+admno = []
+name = []
+
+for i in range (n):
+    ad = input("Enter Admission no: ")
+    nm = input("Enter Name: ")
+    admno.append(ad)
+    name.append(nm)
+
+b = {'Admno':admno,'Name':name}
+c = pd.DataFrame(b)
